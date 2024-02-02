7c6f82a9bbe4832b15d4860aa2495ab1f346ccfd
Mikhail Eshchenko <mikhail.eshchenko@gmail.com>
2024-02-02 15:59:25 +0800

    separate field and apples

--- src/index.ts
+++ src/index.ts
@@ -11,22 +11,39 @@ const SIZE = 10;
 const BORDER_SIZE = 10;
 
 let field0 = [
-    [0,0,0,2,1,0,0,0,0,0],
+    [0,0,0,0,1,0,0,0,0,0],
     [0,1,1,1,1,0,1,1,1,0],
-    [0,1,0,0,0,0,1,2,1,0],
+    [0,1,0,0,0,0,1,0,1,0],
     [0,1,0,1,1,0,1,0,0,0],
-    [0,1,0,1,2,0,1,1,1,1],
+    [0,1,0,1,0,0,1,1,1,1],
     [0,0,0,0,0,0,0,0,0,0],
     [0,1,1,1,1,1,0,1,1,0],
-    [0,1,2,0,0,1,0,1,2,0],
+    [0,1,0,0,0,1,0,1,0,0],
     [0,1,1,1,0,1,0,1,1,1],
     [0,0,0,0,0,1,0,0,0,0]
 ]
 
+let apples0 = [
+    [0,0,0,2,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,2,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,2,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,2,0,0,0,0,0,2,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0]
+]
+
+let start_position = [0, 0];
+let player_position = [0,0];
+
 function clearField(){
     ctx.clearRect(BORDER_SIZE,BORDER_SIZE,canvas.width - BORDER_SIZE*2, canvas.height - BORDER_SIZE*2);
 }
-function drawField(field:number[][]){
+
+function drawField(field:number[][], apples: number[][], player_position:number[]){
     for(let i = 0; i<field.length; i++){
         for(let j = 0; j<field[i].length; j++){
             if(field[j][i] === 0){
@@ -37,8 +54,16 @@ function drawField(field:number[][]){
                 ctx.fillStyle = 'black';
                 ctx.fillRect(i*SIZE + BORDER_SIZE,j*SIZE + BORDER_SIZE,SIZE, SIZE);
                 ctx.fill()
-            } else if(field[j][i] === 2){
+            }
+
+            if(apples[j][i] === 2){
                 ctx.fillStyle = 'blue';
+                ctx.fillRect(i*SIZE + BORDER_SIZE,j*SIZE + BORDER_SIZE,SIZE, SIZE);
+                ctx.fill()
+            }
+
+            if(player_position[0] === i && player_position[1] === j){
+                ctx.fillStyle = 'yellow';
                 ctx.fillRect(i*SIZE + BORDER_SIZE,j*SIZE + BORDER_SIZE,SIZE, SIZE);
                 ctx.fill()
             }
@@ -46,15 +71,30 @@ function drawField(field:number[][]){
     }
 }
 
+document.addEventListener('keydown', (evt:KeyboardEvent) => {
+    console.log(evt.key);
+    if (evt.isComposing || evt.key === '229'){
+        return;
+    }
+    if(evt.key === 'ArrowUp'){
+        player_position[1] === player_position[1] + 1;
+        console.log(player_position)
+    }
+    if(evt.key === 'ArrowDown'){}
+    if(evt.key === 'ArrowLeft'){}
+    if(evt.key === 'ArrowRight'){}
+    if(evt.key === 'Escape'){}
+})
 
 function processInput(){
-    console.log('processInput');
+    // console.log('processInput');
 }
 function changeState(){
-    console.log('changeState');
+    // console.log('changeState');
 }
 function render(){
-    drawField(field0);
+    clearField();
+    drawField(field0, apples0, player_position);
 }
 
 let gameLoop = function(){
